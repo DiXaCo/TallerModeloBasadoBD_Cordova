e3383c8616489b895ead195dbb936a858502ef4c
DiegoCórdovaR <DiegoCrdovaR@DESKTOP-ON931K9>
2023-06-15 23:05:54 -0500

    Agregar archivos de proyecto.

---
+++ Models/CordovaReyesContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TallerModeloBasado.Models
+{
+    public partial class CordovaReyesContext : DbContext
+    {
+        public CordovaReyesContext()
+        {
+        }
+
+        public CordovaReyesContext(DbContextOptions<CordovaReyesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Vehiculo> Vehiculos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; DataBase=CordovaReyes;Trusted_Connection=True; TrustServerCertificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehiculo>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Vehiculo");
+
+                entity.Property(e => e.IdVehiculo).HasColumnName("idVehiculo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Vehiculo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TallerModeloBasado.Models
+{
+    public partial class Vehiculo
+    {
+        public int? IdVehiculo { get; set; }
+    }
+}
